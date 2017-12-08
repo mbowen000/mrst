31938df158739cc19a4995b6ebb7db535d814fdd
MJ <mike.scott.bowen@gmail.com>
2017-12-08 17:27:36 -0600

    Basic boilerplate styles in.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import './styles/Typography.css';
+import './styles/Buttons.css';
+import logo from './assets/logo.svg';
+import robotPlaceholder from './assets/robot-placeholder.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        Test Font
+          <div class="logo">
+            <img src={logo} alt="Robots!"/>
+          </div>
+          <div class="nav">
+            <div class="hamburger">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
         </header>
-       
+        <section class="robots">
+          <div class="robot">
+            <img src={robotPlaceholder} alt="Robot Name"/>
+            <h3>Robot Name</h3>
+            <button class="button primary">Vote</button>
+          </div>
+          <div class="robot">
+            <img src={robotPlaceholder} alt="Robot Name"/>
+            <h3>Robot Name</h3>
+            <button class="button primary" disabled="disabled">Vote</button>
+          </div>
+        </section>
       </div>
     );
   }
